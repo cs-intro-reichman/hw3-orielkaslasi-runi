ee7f5d1a36bd80d119f60db6a00ab140f0be1eb5
Ori Elkaslasi <ori.elkaslasi@post.runi.ac.il>
2025-11-20 17:09:13 +0200

    final 1

--- Anagram.java
+++ Anagram.java
@@ -28,51 +28,35 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		String pro1 = preProcess(str1);
-		String pro2 = preProcess(str2);
-		if (pro1.length() != pro2.length()) {
-			return false;
-		}
-		for (int i = 0; i < pro1.length(); i++) {
-			char ch1 = pro1.charAt(i);
-			boolean found = false;
+		String s1 = preProcess(str1);
+    String s2 = preProcess(str2);
 
-			for (int k = 0; k < pro2.length(); k++) {
-			if (ch1 == pro2.charAt(k)) {
-				pro2 = pro2.substring(0, k) + pro2.substring(k + 1);
-                found = true;
-                break;
-			}
-		}
-			if (!found) {
-				return false;
-			}
-		}
-		return true;
-}	
-	   
+    if (s1.length() != s2.length()) return false;
+
+    char[] a = s1.toCharArray();
+    char[] b = s2.toCharArray();
+
+    java.util.Arrays.sort(a);
+    java.util.Arrays.sort(b);
+
+    return java.util.Arrays.equals(a, b);
+}
+   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		int i = 0;
-		String newStr = "";
-		while (i<str.length()) {
-			char ch = str.charAt(i);
-			
-			if (ch >= 'A' && ch <= 'Z') {
-				newStr = newStr + (char)(ch + 32);
-			}
-			else if (ch >= 'a' && ch <= 'z') {
-				newStr += ch;
-			}
-			else if (ch == ' ') {
-				newStr += ch;
-			}
-			i++;
-		}
-		return newStr;
-	} 
+	
+	StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < str.length(); i++) {
+        char ch = str.charAt(i);
+
+        if (Character.isLetter(ch)) {
+            sb.append(Character.toLowerCase(ch));
+        }
+    }
+    return sb.toString();
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
@@ -87,7 +71,7 @@ public class Anagram {
 
         str = str.substring(0, index) + str.substring(index + 1);
     }
-
+	}
     return anagram;
 }
 }
